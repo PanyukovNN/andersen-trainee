d07f5d5f6078e07862729c01bc79056d7869d085
Nickolay <zylex77@gmail.com>
2020-02-13 15:06:38 +0300

    task 5 fix handler

--- src/main/java/com/company/fifthtask/CacheInvocationHandler.java
+++ src/main/java/com/company/fifthtask/CacheInvocationHandler.java
@@ -1,13 +1,13 @@
 package com.company.fifthtask;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CacheInvocationHandler implements InvocationHandler {
 
-    private final Map<Method, Object> records = new HashMap<>();
+    private final Map<Key, Object> records = Collections.synchronizedMap(new HashMap<>());
 
     private final Object service;
 
@@ -17,14 +17,56 @@ public class CacheInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Object result = records.get(method);
-        if (result == null && !records.containsKey(method)) {
-            result = method.invoke(service, args);
-            Method serviceMethod = service.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes());
-            if (serviceMethod.isAnnotationPresent(Cached.class)) {
-                records.put(method, result);
-            }
+        Key key = new Key(method, args);
+        Method serviceMethod = service.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes());
+        if (serviceMethod.isAnnotationPresent(Cached.class) && !records.containsKey(key)) {
+            records.put(key, invokeMethod(key));
         }
-        return result;
+        return records.get(key);
+    }
+
+    private Object invokeMethod(Key key) {
+        try {
+            return key.getMethod().invoke(service, key.getArgs());
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private class Key {
+
+        private Method method;
+
+        private Object[] args;
+
+        public Key(Method method, Object[] args) {
+            this.method = method;
+            this.args = args;
+        }
+
+        public Method getMethod() {
+            return method;
+        }
+
+        public Object[] getArgs() {
+            return args;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Key key = (Key) o;
+            return Objects.equals(method, key.method) &&
+                    Arrays.equals(args, key.args);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(method);
+            result = 31 * result + Arrays.hashCode(args);
+            return result;
+        }
     }
 }
